20c2721dba1dd778fdccb0a6fb8f2136b49b934e
Richard <emailrchen@gmail.com>
2020-03-18 19:18:25 -0400

    ruby award method

--- update_quality.rb
+++ update_quality.rb
@@ -2,48 +2,65 @@ require 'award'
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
-      if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-        end
-      end
+    case award.name
+    when 'Blue First' 
+      update_blue_first_award!(award)
+    when 'Blue Distinction Plus'
+      update_blue_distinction_plus!(award)
+    when 'Blue Compare'
+      update_blue_compare!(award)
+    when 'Blue Star'
+      update_blue_star!(award)
+    else 
+      update_normal_award!(award)
     end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
-      end
-    end
+  end
+end
+
+def update_blue_distinction_plus!(award)
+  # blue distinction plus awards are not modified
+end
+
+def update_blue_star!(award)
+  award.expires_in -= 1
+
+  if award.expires_in > 0    
+    award.quality = [0, award.quality - 2].max
+  else
+    award.quality = [0, award.quality - 4].max
+  end
+end
+
+def update_normal_award!(award)
+  award.expires_in -= 1 
+
+  if award.expires_in > 0
+    award.quality = [0, award.quality - 1].max
+  else
+    award.quality = [0, award.quality - 2].max
+  end
+end
+
+def update_blue_first_award!(award)
+  award.expires_in -= 1 
+
+  if award.expires_in > 0
+    award.quality = [50, award.quality + 1].min
+  else
+    award.quality = [50, award.quality + 2].min
+  end
+end
+
+def update_blue_compare!(award)
+  award.expires_in -= 1 
+
+  if award.expires_in < 0
+    award.quality = 0
+  elsif award.expires_in < 5
+    award.quality = [50, award.quality + 3].min
+  elsif award.expires_in < 10
+    award.quality = [50, award.quality + 2].min
+  else
+    award.quality = [50, award.quality + 1].min
   end
 end
